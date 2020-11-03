4889cb7c723cc7c55b94565e579628c4fd635fe8
Narayan Krishna <nkrishn8@gmail.com>
2020-11-03 11:50:38 +0530

    added datatypes example

---
+++ datatypes.java
@@ -0,0 +1,21 @@
+// Primitive and Reference datatypes
+
+public class datatypes {
+
+    public static void main (String[] args){
+
+        // Primitive
+        int num = 5;
+        char alpha = 'a';
+        float dec = 0.01f;
+        double numd = 0.001;
+
+        System.out.println("Integer : "+num+", Character : "+alpha+", Float decimal : "+dec+", Double decimal : "+numd);
+        
+        // Reference - Objects, String, Arrays etc
+        String word = "Hello";
+
+        System.out.println("This is string : "+word);
+    }
+
+}
